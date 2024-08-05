d2ab461f5f0d21d331b2409f04a6a4aa5a5849c2
Abderrezague ismail <ki_abderrezague@esi.dz>
2024-08-05 15:38:39 +0100

    initial commit

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ import express, { json } from 'express'
 import mongoose from 'mongoose'
 import userRouter from './routes/user-routes';
 import blogRouter from './routes/blog-routes';
-import { password } from './CONSTANTS';
+import dotenv from 'dotenv';
+
+
+dotenv.config()
 
 const app = express()
 app.use(express.json())
@@ -11,7 +14,7 @@ app.use("/api/user",userRouter)
 app.use("/api/blog",blogRouter)
 
 mongoose.connect(
-    `mongodb://kiabderrezague:${password}@ac-icvxwnq-shard-00-00.o5lshy7.mongodb.net:27017,ac-icvxwnq-shard-00-01.o5lshy7.mongodb.net:27017,ac-icvxwnq-shard-00-02.o5lshy7.mongodb.net:27017/?ssl=true&replicaSet=atlas-iir3qt-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`
+    `mongodb://kiabderrezague:${process.env.PASSWORD}@ac-icvxwnq-shard-00-00.o5lshy7.mongodb.net:27017,ac-icvxwnq-shard-00-01.o5lshy7.mongodb.net:27017,ac-icvxwnq-shard-00-02.o5lshy7.mongodb.net:27017/?ssl=true&replicaSet=atlas-iir3qt-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`
 )
 .then(()=> app.listen(5000))
 .then(()=> console.log("Connected to Mongo DB"))
